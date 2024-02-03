11b8923de25906a453082f93aa2f6ab62f6ddde3
kurodenjiro <kurodenjiro1@gmail.com>
2024-02-03 13:10:45 +0700

    update

--- next.config.js
+++ next.config.js
@@ -9,9 +9,9 @@ const nextConfig = {
         SYMBOL: "ETH",
         RPC: "https://replicator.pegasus.lightlink.io/rpc/v1",
         URL_FAUCET: "https://faucet.pegasus.lightlink.io/",
-        NFT_ADDRESS:'0xBF398Db98d8aBd4847962fCD4d87aB7C8Ec012a6',
-        TOKEN_ADDRESS:'0x151Ca4455aa1c00B145DDd2C94964f646D51cBb1',
-        FAUCET_ADDRESS:'0x373B37C14874d1Cef2eDF278B8046Ce4163Bd62b',
+        NFT_ADDRESS:'0x3C68D401b0082Ed833f6d285Df615254DdDDB5a2',
+        TOKEN_ADDRESS:'0xf898ddC2dBaC71AF98D054fD3DEE0284f4aB9483',
+        FAUCET_ADDRESS:'0x17D36ECA9CFA04C7435FE60447091C0872733c47',
         EXPLORER_URL:'https://pegasus.lightlink.io'
       },
 }
